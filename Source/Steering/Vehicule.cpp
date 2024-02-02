560ee3eb5bd523f30103cb477ba3ed2282f4491c
Dorian Barsoni <dbarsoni@gmail.com>
2024-02-02 11:32:56 +0100

    Trying to do orientation

--- Source/Steering/Vehicule.cpp
+++ Source/Steering/Vehicule.cpp
@@ -1,5 +1,7 @@
 #include "Vehicule.h"
 #include "Math/UnrealMathUtility.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "MathUtil.h"
 
 AVehicule::AVehicule() : mass(1), velocity(0), max_force(10), max_speed(10) {
 	PrimaryActorTick.bCanEverTick = true;
@@ -42,7 +44,10 @@ void AVehicule::calculateNewPosition(FVector steering_direction) {
 }
 
 void AVehicule::caculateNewOrientation() {
-
+	UE_LOG(LogTemp, Warning, TEXT("Velocity : %s"), *(velocity).ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Velocity normal : %s"), *(velocity.GetSafeNormal()).ToString());
+	orientation.SetColumn(0, velocity.GetSafeNormal());
+	UE_LOG(LogTemp, Warning, TEXT("Column 0 : %s"), *(orientation.GetColumn(0)).ToString());
 	/*
 		new_forward = normalize(velocity);
 		approximate_up = normalize(approximate_up); // if needed
@@ -51,14 +56,17 @@ void AVehicule::caculateNewOrientation() {
 	*/
 }
 
-
-
 void AVehicule::seek(AActor *target) {
 	FVector desired_velocity = (target->GetActorLocation() - this->GetActorLocation()).GetSafeNormal() * max_speed;
 	FVector steering = desired_velocity - velocity;
 
 	calculateNewPosition(steering);
+	caculateNewOrientation();
+
 	this->SetActorLocation(position);
+	float theta = UKismetMathLibrary::Acos(orientation.GetColumn(0).X);
+	UE_LOG(LogTemp, Warning, TEXT("angle : %f"), theta * 180 / PI);
+	this->SetActorRotation(FRotator(0, theta * 180 / PI, 0));
 }
 
 
